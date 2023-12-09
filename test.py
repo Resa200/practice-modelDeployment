e7c858903bdeb46df473d9a4e92211d49cd7781c
Resa200 <180401046@live.unilag.edu.ng>
2023-12-09 13:23:16 +0100

    done!

---
+++ test.py
@@ -0,0 +1,22 @@
+import requests
+
+# Define the URL of your Flask API
+api_url = "https://472d-102-88-62-95.ngrok-free.app/predict"
+
+# Sample text for testing
+sample_texts = ["You people should improve in the item's you ", "Hate this", "kindly improve this", "I just don't like this"]
+
+# Create a dictionary with the text
+for sample_text in sample_texts:
+    data = {"text": sample_text}
+
+    # Send a POST request to the API
+    response = requests.post(api_url, json=data)
+
+    # Check the response status code
+    if response.status_code == 200:
+        # Print the JSON response
+        print(response.json())
+    else:
+        # Print an error message if the request was not successful
+        print(f"Error: {response.status_code}, {response.text}")
